0c70e4548956aa19caf6c8af173ba465de0e490e
yoav_a <yoav_a@WS48.cs.colman.ac.il>
2017-01-16 17:17:42 +0200

    Moved the "Command" package to "Controller" package

---
+++ Sokoban/src/controller/CommandsQueue.java
@@ -0,0 +1,17 @@
+package controller;
+
+import java.util.concurrent.BlockingQueue;
+
+import controller.commands.Command;
+
+public class CommandsQueue {
+	Command commands;
+	BlockingQueue<Command>  commandsQueue;
+	
+	void inserCommand(Command command){
+		
+	}
+	void start(){}
+	void stop(){}
+
+}
--- Sokoban/src/model/MyModel.java
+++ Sokoban/src/model/MyModel.java
@@ -2,6 +2,13 @@ package model;
 
 import java.util.Observable;
 
+import model.data.Level;
+
 public class MyModel extends Observable implements Model {
 	
+//	Level getLevel(){
+//		Level lvl;
+//		
+//		return lvl;		
+//	}
 }
